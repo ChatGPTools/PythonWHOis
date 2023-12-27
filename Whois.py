af65553397a856a96c17ea5f6367974cea1f0280
ChatGPTools <154993936+ChatGPTools@users.noreply.github.com>
2023-12-27 20:44:55 +0100

    Update Whois.py

--- Whois.py
+++ Whois.py
@@ -2,8 +2,8 @@ import socket
 import requests
 from ipwhois import IPWhois
 import scapy.all as scapy
-from scapy.layers.inet import IP, ARP, Ether, TCP
-from scapy.sendrecv import sr1
+from scapy.layers.l2 import ARP, Ether
+from scapy.layers.inet import IP, TCP
 
 def get_basic_info(ip_address):
     try:
@@ -67,14 +67,54 @@ def scan(ip_range):
         devices_list.append(device_dict)
     return devices_list
 
-def scan_ports(ip_address, ports, timeout=1):
+def scan_ports(ip_address, ports, timeout=1, scan_speed="normal"):
     open_ports = []
+
+    print(f"\nScansione delle porte su {ip_address} in corso...")
+
     for port in ports:
         packet = IP(dst=ip_address)/TCP(dport=port, flags="S")
-        response = sr1(packet, timeout=timeout, verbose=False)
+        response = scapy.sr1(packet, timeout=timeout, verbose=False)
+
         if response and response.haslayer(TCP) and response.getlayer(TCP).flags == 0x12:
             open_ports.append(port)
-    return open_ports
+
+        if scan_speed == "fast":
+            print(f"Porta {port} controllata")
+
+    print("\nScansione completata.")
+
+    if open_ports:
+        print("\nPorte aperte:")
+        for port in open_ports:
+            print(f"Porta {port} aperta")
+    else:
+        print("\nNessuna porta aperta trovata.")
+
+def port_scanning_menu(ip_address):
+    print("\nOpzioni di scansione delle porte:")
+    print("1. Scansione veloce")
+    print("2. Scansione normale")
+    print("3. Scansione personalizzata")
+    print("4. Torna al menu principale")
+
+    choice = input("Scelta: ")
+
+    if choice == "1":
+        ports_to_scan = range(1, 1025)
+        scan_ports(ip_address, ports_to_scan, scan_speed="fast")
+    elif choice == "2":
+        ports_to_scan = range(1, 1025)
+        scan_ports(ip_address, ports_to_scan)
+    elif choice == "3":
+        custom_ports = input("Inserisci l'intervallo di porte da scansionare (es. 1-100): ")
+        start_port, end_port = map(int, custom_ports.split('-'))
+        ports_to_scan = range(start_port, end_port + 1)
+        scan_ports(ip_address, ports_to_scan)
+    elif choice == "4":
+        return
+    else:
+        print("Scelta non valida. Riprova.")
 
 if __name__ == "__main__":
     target_ip = input("Inserisci l'indirizzo IP da cui iniziare la scansione: ")
@@ -89,36 +129,31 @@ if __name__ == "__main__":
     for device in devices:
         print(f"{device['ip']}\t\t{device['mac']}")
 
-    target_device_ip = input("Inserisci l'IP del dispositivo per la scansione delle porte: ")
+    while True:
+        print("\nMenu principale:")
+        print("1. Informazioni di base sull'IP")
+        print("2. Informazioni sulla posizione dell'IP")
+        print("3. Informazioni WHOIS sull'IP")
+        print("4. Tutte le informazioni")
+        print("5. Scansione delle porte")
+        print("6. Esci")
 
-    ports_to_scan = range(1, 1025)  # Puoi modificare l'intervallo delle porte se necessario
-    open_ports = scan_ports(target_device_ip, ports_to_scan)
+        main_choice = input("Scelta: ")
 
-    print("\nOpzioni:")
-    print("1. Informazioni di base sull'IP")
-    print("2. Informazioni sulla posizione dell'IP")
-    print("3. Informazioni WHOIS sull'IP")
-    print("4. Tutte le informazioni")
-    print("5. Scansione delle porte")
-
-    choice = input("Scelta: ")
-
-    if choice == "1":
-        get_basic_info(target_ip)
-    elif choice == "2":
-        get_location_info(target_ip)
-    elif choice == "3":
-        get_whois_info(target_ip)
-    elif choice == "4":
-        get_basic_info(target_ip)
-        get_location_info(target_ip)
-        get_whois_info(target_ip)
-    elif choice == "5":
-        if open_ports:
-            print("\nPorte aperte:")
-            for port in open_ports:
-                print(f"Porta {port} aperta")
+        if main_choice == "1":
+            get_basic_info(target_ip)
+        elif main_choice == "2":
+            get_location_info(target_ip)
+        elif main_choice == "3":
+            get_whois_info(target_ip)
+        elif main_choice == "4":
+            get_basic_info(target_ip)
+            get_location_info(target_ip)
+            get_whois_info(target_ip)
+        elif main_choice == "5":
+            port_scanning_menu(target_ip)
+        elif main_choice == "6":
+            print("Programma terminato.")
+            break
         else:
-            print("\nNessuna porta aperta trovata.")
-    else:
-        print("Scelta non valida.")
+            print("Scelta non valida. Riprova.")
